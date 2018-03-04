7bdf0a03105a4384eeed7ff1f7cd3ce35fe48317
Kien Nguyen <kdot@Kiens-MacBook-Pro.local>
2018-03-04 13:22:48 -0800

    Pixel Art Maker v1

---
+++ design.js
@@ -0,0 +1,59 @@
+
+let gridH, gridW, color;
+let table = $('#pixelCanvas');
+
+
+function makeGrid(gridH,gridW){
+	const tbody = $("<tbody></tbody>");	
+	
+	for(let x = 0; x < gridH; x++) {		
+		let newRow = $('<tr class = "col"></tr>');
+		for (let y = 0; y < gridW; y++) {
+			let newCell = $('<td class = "cell"></td>');	
+			$(newRow).append(newCell);			
+			}	
+		$(tbody).append(newRow);	
+		}
+	table.append(tbody);
+	};
+
+
+
+
+$('#sizePicker').submit(function( event ) {
+ 		table.children().remove();
+ 		event.preventDefault();
+ 		gridH = $('#inputHeight').val();
+ 		gridW = $('#inputWidth').val(); 	
+ 		color = $('#colorPicker').val();
+ 		makeGrid(gridH,gridW);
+ 		fillCell();
+ 		});
+ 		 
+function fillCell(){
+	color = $('#colorPicker').val();
+	$('td').on('click',function(){
+		$(this).css('background-color', color ) ;
+	})};
+
+
+
+
+
+
+
+
+
+
+
+
+ 		
+
+ 	
+
+
+
+
+
+
+
